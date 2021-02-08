f8d1b78b69b794f7c34963e4affff0913dfce5bf
KatherineYanes <62476969+KatherineYanes@users.noreply.github.com>
2021-02-08 17:50:08 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string op = comboBox1.Text;
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            double r = 0;
+
+            switch (op)
+            { 
+                case "Suma":
+                    r = num1 + num2;
+                    break;
+
+                case "Resta":
+                    r = num1 - num2;
+                    break;
+
+                case "Multiplicación":
+                    r = num1 * num2;
+                    break;
+
+                case "División":
+                    if (num2 != 0)
+                    {
+                        r = num1 / num2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se puede dividir entre 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                    }
+
+                    break;
+
+            }
+
+            textBox3.Text = r.ToString();
+
+
+        }
+
+
+
+        
+    }
+}
